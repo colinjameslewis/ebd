73220b60c0a2fff9d415fb8603a6d80eca9c2395
Nick Ristine <Nick Ristine>
2016-11-09 13:33:31 -0500

    Contacts Page Updates

--- Articles/index.php
+++ Articles/index.php
@@ -16,7 +16,7 @@
 <section id="articles">
 	<div class="wrap clearfix">
 	
-		<h2 class="roboto-thin ebd-maroon text-center">Our President, Ted Rosen, has written extensively on various topics regarding the sales process within the financial services industry and his articles have been published in numerous acclaimed financial journals.</h2> 
+		<h2 class="roboto-thin text-center">Our President, <span class="text-primary">Ted Rosen</span>, has written extensively on various topics regarding the sales process within the financial services industry and his articles have been published in numerous acclaimed financial journals.</h2> 
 	
 
 		<div id="article-container" class="clearfix">
@@ -27,12 +27,12 @@
 			echo 
 			'<a class="article clearfix row" href="' . articles_URL . $article["pdf"] . '" target="_blank">'
 				.
-				'<div class="article-icon col-sm-4 text-center">'
+			'<div class="article-icon col-sm-4 text-center">'
 					.
 					'<img class="logo-center " src="' . img_URL . $article["logo"] . '" />'
 					.
 					
-				'</div>'	
+				'</div>'
 				.
 				'<div class="title-text roboto-thin col-sm-8">'			
 					.
--- Contact/index.php
+++ Contact/index.php
@@ -13,18 +13,307 @@
 
 
 
-<div>
-
-CONTACT US Now
-
-</div>	
 
 
 
 
+<!-- Contact Icon Banner -->
+	<div id="contact-icon-banner" class="col-sm-12">
+
+		<div class="contact-icon col-sm-4">
+			<p class="icon-text email">
+			<img src ="/assets/img/Icons/contactemail.png" alt ="<blank>" style ="width:30px;">
+		
+				<br>
+				info@expertbizdev.com
+			</p>
+		</div>
+		<div class="contact-icon phone col-sm-4">
+			<p class="icon-text" id="phone-text"><span id="call-us">
+			Call Us!<br></span>
+				<img id="phone-icon" src ="/assets/img/Icons/headerphone-gray.png"  alt ="Call Us!" >
+				800-422-1066</p>
+		</div>
+		<div class="contact-icon  col-sm-4">		
+			<p class="icon-text">
+			<img src ="/assets/img/Icons/fax.png" alt ="<blank>" style ="width:33px; padding-left:3px;"><br>
+				610-680-3360
+			</p>
+		</div>
+	</div>	
+	
+<section id="Contact">
+<div class="wrap clearfix">
+
+		<h3 class="roboto-thin text-center">We are always available to speak with prospective clients regarding their businesses and sales needs. Contact us today to begin planning your next campaign!</h3> 
+		
+		
+	
+	
+	
+	<!-- Contact Boxes -->
+	<div id="Contact-Container" class="col-sm-12" >		
+		
+		<!-- ****** SALES ****** -->
+			<div class="Contact-Box col-sm-4">
+				<div class="inner-contact col-sm-12">
+				<h3 class="Department-Header"> Sales </h3>
+				<div class="Contact-info col-sm-12">
+					<table class="contact-table">
+						<tr class="contact-name">
+							<td>
+							Ted Rosen
+							<td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon"  src ="/assets/img/Icons/contactemail-gray.png" alt ="<blank>" >tarosen@expertbizdev.com
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon" src ="/assets/img/Icons/headerphone-gray.png"  alt ="Call Us!" >610-771-2121
+							</td>
+						</tr>
+					</table>
+				</div>
+			
+				<div class="Contact-info col-sm-12">
+					<table class="contact-table">
+						<tr class="contact-name">
+							<td>
+							Matt Regn
+							<td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon"  src ="/assets/img/Icons/contactemail-gray.png" alt ="<blank>">mregn@expertbizdev.com
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon" src ="/assets/img/Icons/headerphone-gray.png"  alt ="Call Us!" >484-429-7150
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>	
+			</div>	
+				
+				<!-- ****** Client Services ******* --->
+				
+			<div class="Contact-Box col-sm-4">
+			<div class="inner-contact col-sm-12">
+				<h3 class="Department-Header"> Client Services </h3>
+				<div class="Contact-info col-sm-12">
+					<table class="contact-table">
+						<tr class="contact-name">
+							<td>
+							Judy Friedman
+							<td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon"  src ="/assets/img/Icons/contactemail-gray.png" alt ="<blank>" >jfriedman@experbizdev.com
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon" src ="/assets/img/Icons/headerphone-gray.png"  alt ="Call Us!" >610-771-2123
+							</td>
+						</tr>
+					</table>
+				</div>
+				
+				<div class="Contact-info col-sm-12">
+					<table class="contact-table">
+						<tr class="contact-name">
+							<td>
+							Dolores Recchiute
+							<td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon"  src ="/assets/img/Icons/contactemail-gray.png" alt ="<blank>" >drecch@expertbizdev.com
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon" src ="/assets/img/Icons/headerphone-gray.png"  alt ="Call Us!" >610-660-4902
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>	
+			</div>
+				
+				
+			<!-- ****** ELMS  ******  -->
+				
+		<div class="Contact-Box col-sm-4" >	
+			<div class="inner-contact col-sm-12">
+				<h3 class="Department-Header col-sm-12"> ELMS </h3>
+				<div class="Contact-info col-sm-12">
+					<table class="contact-table">
+						<tr class="contact-name">
+							<td>
+							
+						<b>Support</b> - Michael Usavage
+							<td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon"  src ="/assets/img/Icons/contactemail-gray.png" alt ="<blank>" >musavage@expertbizdev.com
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon" src ="/assets/img/Icons/headerphone-gray.png"  alt ="Call Us!" >610-771-2120
+							</td>
+						</tr>
+					</table>
+				</div>
+				
+				<div class="Contact-info col-sm-12">
+					<table class="contact-table">
+						<tr class="contact-name">
+							<td>
+							<b>Sales </b> - Matt Regn
+							<td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon"  src ="/assets/img/Icons/contactemail-gray.png" alt ="<blank>">mregn@expertbizdev.com
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon" src ="/assets/img/Icons/headerphone-gray.png"  alt ="Call Us!" >484-429-7150
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>	
+			
+	
+				<!-- ****** eMarketing ****** -->
+				
+			<div class="Contact-Box col-sm-4">
+			<div class="inner-contact col-sm-12">
+					<h3 class="Department-Header"> eMarketing</h3>
+				<div class="Contact-info col-sm-12">
+					<table class="contact-table">
+						<tr class="contact-name">
+							<td>
+							Colin Lewis
+							<td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon"  src ="/assets/img/Icons/contactemail-gray.png" alt ="<blank>">clewis@expertbizdev.com
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon" src ="/assets/img/Icons/headerphone-gray.png"  alt ="Call Us!" >610-771-2128
+							</td>
+						</tr>
+					</table>
+				</div>
+				</div>
+			</div>	
+				
+			<div class="Contact-Box col-sm-4">	
+			<div class="inner-contact col-sm-12">
+						<h3 class="Department-Header"> Recruiting</h3>
+				<div class="Contact-info col-sm-12">
+					<table class="contact-table">
+						<tr class="contact-name">
+							<td>
+							Renea Holman-Grogan
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon"  src ="/assets/img/Icons/contactemail-gray.png" alt ="<blank>" >rgrogan@expertbizdev.com
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon" src ="/assets/img/Icons/headerphone-gray.png"  alt ="Call Us!" >253-566-2221
+							</td>
+						</tr>
+					</table>
+				</div>
+				</div>
+				</div>
+		
+			<!-- ****** Tech Support ****** -->
+			
+			
+			<div class="Contact-Box col-sm-4">	
+			<div class="inner-contact col-sm-12">
+						<h3 class="Department-Header">Tech Support</h3>
+				<div class="Contact-info col-sm-12">
+					<table class="contact-table">
+						<tr class="contact-name">
+							<td>
+							Michael Usavage
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon"  src ="/assets/img/Icons/contactemail-gray.png" alt ="<blank>" >musavage@expertbizdev.com
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<img class="small-icon" src ="/assets/img/Icons/headerphone-gray.png"  alt ="Call Us!" >610-771-2120
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- General Inquires Banner -->
+	<div id="general-inq">
+		<button id="gen-inq-button">
+		General Inquires 
+		</button>
+	</div>
+	
+	
+
+	
+	<!-- Map Box --> 
+	<div id="map-box" >
+		<div id="map-info" class="col-sm-12">		
+					<span class="glyphicon glyphicon-map-marker"></span>
+					Office Location</br>			
+					<p id ="address-info">3 Bala Plaza East, Suite 504 </br>
+					Bala Cynwyd, PA 19004 </p>
+		</div>
+		<div id="Map-center" class="col-sm-12"> 		
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3055.986326276617!2d-75.21939509999999!3d40.00875300000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6b89e0d320c13%3A0xd5e27f81b469eef5!2s3+Bala+Plaza+%23504%2C+Bala+Cynwyd%2C+PA+19004!5e0!3m2!1sen!2sus!4v1418411011841" frameborder="0" width="100%" ></iframe>
+		</div>  
+		</div>
+		
+		
+	
+	
+
+	
+</div>
+</section>	
 
 
-yl
+
+
+
+
+
 <?php 
 
 	include(ROOT_PATH . "inc/footer.php");
